1b0098898eca8989d63dc13382f91092836dfa93
Frejus Adedemi <frejusadedemi2@gmail.com>
2025-04-21 18:02:56 +0200

    Fix

--- static/script.js
+++ static/script.js
@@ -95,12 +95,6 @@ document.addEventListener("DOMContentLoaded", () => {
           <td>${entry.nb_tentatives}</td>
           <td>${entry.ports.join(", ")}</td>
         `;
-          row.classList.add(
-          threat_score > 80 ? "danger" :
-          threat_score > 50 ? "warning" :
-          "safe"
-        );
-
         body.appendChild(row);
       });
 
